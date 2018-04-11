ca7aa8cf8013b1f0a087e9a044167f8294822d28
Elliot Potts <elliot@skyzun.net>
2018-04-11 15:50:48 +0100

    Library imports and comments

---
+++ main.py
@@ -0,0 +1,12 @@
+# 'FrameRipper', Python project to pull still images from a video.
+# Written by Elliot Potts: https://github.com/Elliot-Potts :D
+
+import time
+import sys
+import cv2
+import os
+
+
+
+
+
