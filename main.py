1da7b353bfdcc6f1409aa3c4feecfc0b8195d882
Elliot Potts <elliot@skyzun.net>
2018-04-11 16:09:55 +0100

    Added the settings and logger initialisation function.
    Added the logging and settings directory init function.

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 # 'FrameRipper', Python project to pull still images from a video.
 # Written by Elliot Potts: https://github.com/Elliot-Potts :D
 
+import configparser
+import logging
+import datetime
 import time
 import sys
 import cv2
 import os
 
+getDate = datetime.datetime.now().strftime("%Y-%m-%d-%H-%S-%f")
+logging.basicConfig(filename=(getDate+"_frameRipper.log",),
+                    level=logging.DEBUG,
+                    format='%(levelname)s - %(asctime)s - %(message)s - ')
 
 
+def settingsInit():
+    if os.path.isfile("C:\Potts' Software\FrameRipper\Logging"):
+        logging.info("Logging directory found. Continuing.")
+    else:
+        os.makedirs("C:\Potts' Software\FrameRipper\Logging")
+        logging.warning("No logging directory found. Created one and started logger.")
 
+    if os.path.isfile("C:\Potts' Software\FrameRipper\Settings"):
+        logging.info("Located settings directory.")
+    else:
+        logging.warning("No settings directory found. Creating one.")
+        os.makedirs("C:\Potts' Software\FrameRipper\Settings")
+
+
+settingsInit()
 
